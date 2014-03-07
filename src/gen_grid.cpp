a2cc788a969db3b4b03c4e720fe5a8a888a11992
Neil Massey <nrmassey@yahoo.co.uk>
2014-03-07 15:43:58 +0000

    Added perimeter argument to trim values from sponge layer of regional models

--- src/gen_grid.cpp
+++ src/gen_grid.cpp
@@ -21,7 +21,7 @@
 int main(int argc, char** argv)
 {
 	// set up the command line arguments
-	int max_I, init_S, max_pts, max_levs;
+	int max_I, init_S, max_pts, max_levs, perim;
 	std::string out_name = "";
 	std::string in_name = "";
 	std::string var_name = "";
@@ -37,7 +37,8 @@ int main(int argc, char** argv)
 		TCLAP::SwitchArg text_out_arg("T", "text", "Output grid in text format, as well as the binary format", cmd, false);
 		TCLAP::SwitchArg lat_lon_out_arg("L", "lat_lon", "Output grid in text latitude / longitude coordinates, as well as the binary format", cmd, false);
 		TCLAP::SwitchArg stats_arg("S", "stats", "Show statistics for the mesh", cmd, false);
-		TCLAP::ValueArg<int> init_shp_arg("i", "init_shape", "Initialisation shape: 0 = ICOSAHEDRON, 1 = OCTAHEDRON, 3 = DYMAXION(tm)", false, 0, "int", cmd);
+		TCLAP::ValueArg<int> perim_arg("p", "perimeter", "Number of grid boxes to trim from the perimeter when generating mesh", false, 0, "int", cmd);
+		TCLAP::ValueArg<int> init_shp_arg("i", "init_shape", "Initialisation shape: 0 = ICOSAHEDRON, 1 = OCTAHEDRON, 2 = DYMAXION(tm)", false, 0, "int", cmd);
 		TCLAP::ValueArg<int> max_its_arg("I", "max_its", "Maximum number of iterations in equalisation phase", false, 1e3, "int", cmd);
 		TCLAP::ValueArg<int> max_pts_arg("n", "max_pts", "Maximum number of points from the original grid allowed per triangle", false, 0, "int", cmd);
 		TCLAP::ValueArg<int> max_levs_arg("l", "max_level", "Maximum number of levels allowed in the grid", false, -1, "int", cmd);
@@ -52,6 +53,7 @@ int main(int argc, char** argv)
 		text_out = text_out_arg.getValue();
 		save_text = lat_lon_out_arg.getValue();
 		stats = stats_arg.getValue();
+		perim = perim_arg.getValue();
 		out_name = out_name_arg.getValue();
 		var_name = var_name_arg.getValue();
 		in_name = in_name_arg.getValue();
@@ -79,7 +81,7 @@ int main(int argc, char** argv)
 	{
 		ncdata nc_data(in_name, var_name);
 		tri_grid tg;
-		tg.initialize(SHAPE(init_S), &nc_data, max_pts, max_levs, max_I);
+		tg.initialize(SHAPE(init_S), &nc_data, max_pts, max_levs, max_I, perim);
 		tg.save(out_name);
 		std::cout << "# Saved mesh to file: " << out_name.c_str() << std::endl;
 		if (save_text)
