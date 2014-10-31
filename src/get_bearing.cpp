f24364135b5e97ea402ee152632908992559916b
nrmassey <nrmassey@yahoo.co.uk>
2014-10-31 15:14:38 +0000

    Fixed get bearing
    Calculation was actually wrong previously!

--- src/get_bearing.cpp
+++ src/get_bearing.cpp
@@ -13,14 +13,17 @@ FP_TYPE get_bearing(FP_TYPE lon1, FP_TYPE lat1, FP_TYPE lon2, FP_TYPE lat2)
 {
     // get the bearing between two lon / lat pairs
     const FP_TYPE deg_to_rad = M_PI / 180.0;
-    FP_TYPE nlat1 = lat1 * deg_to_rad;
-    FP_TYPE nlat2 = lat2 * deg_to_rad;
-    FP_TYPE dlon  = (lon2 - lon1) * deg_to_rad;
-    FP_TYPE a = sin(nlat1)*sin(nlat2) + cos(nlat1)*cos(nlat2)*cos(dlon);
-    FP_TYPE ang = 0.0;
-//    if (a >= -1.0 and a <= 1.0)
-        ang = acos(a);
-    return ang;
+    const FP_TYPE rad_to_deg = 180.0 / M_PI;
+   
+	FP_TYPE nlat1 = lat1 * deg_to_rad;
+	FP_TYPE nlat2 = lat2 * deg_to_rad;
+	FP_TYPE dlon  = (lon2 - lon1) * deg_to_rad;
+	
+	FP_TYPE a = sin(dlon) * cos(nlat2);
+	FP_TYPE b = cos(nlat1) * sin(nlat2) - sin(nlat1) * cos(nlat2) * cos(dlon);
+	FP_TYPE ang = atan2(a,b);
+	ang = ang * rad_to_deg;
+	return ang;  // returned in degrees;
 }
 
 /*****************************************************************************/
